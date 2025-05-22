3870967607274adce29bc30163df1aec7e091781
trangdh7 <trangdh176168@gmail.com>
2025-05-22 15:59:29 +0700

    delete

--- baogiaphong-backend/models/Keyword.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const keywordSchema = new mongoose.Schema({
-  keyword: String,
-  created_at: Date
-});
-
-module.exports = mongoose.model('Keyword', keywordSchema);
